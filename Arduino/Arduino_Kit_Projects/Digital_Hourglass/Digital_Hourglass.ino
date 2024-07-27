8dff52fd810ba113a8ad59006bf2979152c7c6cd
Akkun716 <adon.anglon@gmail.com>
2024-07-27 07:30:16 -0700

    Initial commit

---
+++ Arduino/Arduino_Kit_Projects/Digital_Hourglass/Digital_Hourglass.ino
@@ -0,0 +1,13 @@
+/**
+ * The program is a timer that uses LEDs as time markers until all LEDs are
+ * illuminated. The timer can be reset with the use of a switch (a push button
+ * switch in this case)
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
