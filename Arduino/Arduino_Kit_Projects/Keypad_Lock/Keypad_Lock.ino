0d017d3d673ebc0bd3b53ca8218955d0d5c42064
Akkun716 <adon.anglon@gmail.com>
2024-09-06 16:49:54 -0700

    Relocated some code and added more thorough comments

--- Arduino/Arduino_Kit_Projects/Keypad_Lock/Keypad_Lock.ino
+++ Arduino/Arduino_Kit_Projects/Keypad_Lock/Keypad_Lock.ino
@@ -1,3 +1,9 @@
+/****
+ * This program utilizes a servo to simulate a locking mechanism.
+ * A push switch is used to set the servo to "locked" and keypad
+ * with a password set during the startup of the arduino to unlock.
+ ****/
+
 #include <Servo.h>
 #include <Key.h>
 #include <Keypad.h>
@@ -5,14 +11,19 @@
 // Keypad variables
 const int ROW_COUNT = 4;
 const int COL_COUNT = 3;
-const char KEY_LAYOUT[ROW_COUNT][COL_COUNT]
-	= {{'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}};
+const char KEY_LAYOUT[ROW_COUNT][COL_COUNT] = {
+{'1', '2', '3'},
+{'4', '5', '6'},
+{'7', '8', '9'},
+{'*', '0', '#'}
+};
 const byte ROW_PIN[ROW_COUNT] = {A2, A3, A4, A5};
 const byte COL_PIN[COL_COUNT] = {4, 3, 2};
 
 // Input components
 Servo mainServo;
-Keypad mainKeypad = Keypad(makeKeymap(KEY_LAYOUT), ROW_PIN, COL_PIN, ROW_COUNT, COL_COUNT);
+Keypad mainKeypad =
+	Keypad(makeKeymap(KEY_LAYOUT), ROW_PIN, COL_PIN, ROW_COUNT, COL_COUNT);
 const int switchPin = A0;
 
 // Output components
@@ -21,14 +32,13 @@ const int yellowLED = 6;
 const int redLED = 9;
 
 // Digital variables
-const int PASSWORD_LEN = 4;
-int password[PASSWORD_LEN];
-int userEntry[4];
-char keyPress;
-const int FAST_BLINK = 125;
+// Set speeds for LED blinks
+const int FAST_BLINK = 50;
 const int MED_BLINK = 250;
+const int PASSWORD_LEN = 4;
+char password[PASSWORD_LEN];
+char userEntry[PASSWORD_LEN];
 bool locked = false;
-int switchVal;
 int led[1] = {};
 
 void setup() {
@@ -39,30 +49,12 @@ void setup() {
 	mainServo.attach(11);
 	Serial.begin(9600);
 	
-	// Set the password
-	digitalWrite(greenLED, HIGH);
-	digitalWrite(yellowLED, HIGH);
-	digitalWrite(redLED, HIGH);
-	Serial.print("Waiting for 4 key password entry: ");
-	
-	int leds[3] = {greenLED, yellowLED, redLED};
-	userKeypadEntry(leds, 3, true);
-	for(int i = 0; i < 4; i++) {
-		password[i] = userEntry[i];
-	}
-	
-	// Flash all LEDs to indicate password initialization
-	flashLEDs(leds, 3, 2, MED_BLINK, false);
-	Serial.println("\nPassword set");
-	
-	digitalWrite(greenLED, HIGH);
-	mainServo.write(0);
-	Serial.println("Servo: unlocked");
+	setPassword();
 }
 
 void loop() {
 	if(!locked) {
-		switchVal = digitalRead(switchPin);
+		int switchVal = digitalRead(switchPin);
 		if(switchVal == HIGH) {
 			// If switch is engaged, power redLED and set
 			// servo to "locked" position
@@ -71,7 +63,7 @@ void loop() {
 			digitalWrite(redLED, HIGH);
 			mainServo.write(90);
 			Serial.print("Servo: locked");
-			delay(1000);
+			delay(10);
 		}
 	}
 	if(locked) {
@@ -95,15 +87,22 @@ void loop() {
 }
 
 /**
- * User enters a 4 character sequence in the keypad.
+ * User enters a valid length character sequence in the keypad. The program
+ * waits until the user enters enough characters.
+ *
+ * @param leds An integer list of LED pins to be affected
+ * @param arrLength An integer representing the length of the leds array
+ * @param endHigh A boolean determining if LEDs end on HIGH or LOW
  **/
 void userKeypadEntry(int leds[], int arrLength, bool endHigh) {
-	for(int i = 0; i < 4; i++) {
+	char keyPress;
+	for(int i = 0; i < PASSWORD_LEN; i++) {
 		keyPress = NO_KEY;
 		while(keyPress == NO_KEY) {
 			keyPress = mainKeypad.getKey();
 		}
 		userEntry[i] = keyPress;
+		// LEDs indicate a key press was accepted
 		flashLEDs(leds, arrLength, 1, FAST_BLINK, endHigh);
 		Serial.print(keyPress);
 	}
@@ -111,9 +110,12 @@ void userKeypadEntry(int leds[], int arrLength, bool endHigh) {
 
 /**
  * Checks the keypad entry to the set password.
+ *
+ * @param entry A char array which holds the entry to be checked
+ * @param password A char array that holds the saved password
  **/
-bool checkKeyEntry(int entry[], int password[]) {
-	for(int i = 0; i < 4; i++) {
+bool checkKeyEntry(char entry[], char password[]) {
+	for(int i = 0; i < PASSWORD_LEN; i++) {
 		if(entry[i] != password[i]) {
 			return false;
 		}
@@ -123,7 +125,13 @@ bool checkKeyEntry(int entry[], int password[]) {
 }
 
 /**
- * Flashes the specified leds a certain number of times
+ * Flashes the specified leds a certain number of times.
+ * 
+ * @param leds An integer list of LED pins to be affected
+ * @param arrLength An integer representing the length of the leds array
+ * @param count An integer for number of LED blinks
+ * @param blinkSpeed An integer representing blink delay
+ * @param endHigh A boolean determining if LEDs end on HIGH or LOW
  **/
 void flashLEDs(int leds[], int arrLength, int count, int blinkSpeed, bool endHigh) {
 	count += endHigh;
@@ -144,4 +152,31 @@ void flashLEDs(int leds[], int arrLength, int count, int blinkSpeed, bool endHig
 		
 		count--;
 	}
+}
+
+/**
+ * Enables the user to enter a PASSWORD_LEN char password in the keypad.
+ * After the password is set, the servo moves to the unlocked position.
+ **/
+void setPassword() {
+	// All LEDs will be activated and respond to key presses to highlight
+	// the fact the password is being reset
+	digitalWrite(greenLED, HIGH);
+	digitalWrite(yellowLED, HIGH);
+	digitalWrite(redLED, HIGH);
+	Serial.print("Waiting for 4 key password entry: ");
+	
+	int leds[3] = {greenLED, yellowLED, redLED};
+	userKeypadEntry(leds, 3, true);
+	for(int i = 0; i < PASSWORD_LEN; i++) {
+		password[i] = userEntry[i];
+	}
+	
+	// Flash all LEDs to indicate password initialization
+	flashLEDs(leds, 3, 2, MED_BLINK, false);
+	Serial.println("\nPassword set");
+	
+	digitalWrite(greenLED, HIGH);
+	mainServo.write(0);
+	Serial.println("Servo: unlocked");
 }
